c9bc11f302aab67f5ebb3c6566ad2ee47adf3a87
Jesus Barajas <jesus.barajas@ordermygear.com>
2021-05-25 12:30:07 -0600

    Login with Env variables and secret pass

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -1,13 +1,4 @@
-/// <reference types="cypress" />
-
-context('Actions', () => {
-  beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/actions')
-  })
-
-  // https://on.cypress.io/interacting-with-elements
-
-  it('.type() - type into a DOM element', () => {
+it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
     cy.get('.action-email')
       .type('fake@email.com').should('have.value', 'fake@email.com')
